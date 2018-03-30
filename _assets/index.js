102af71c9be3dbce7e0975b14a187462366b5070
Guido Flohr <guido.flohr@cantanea.com>
2018-03-30 13:55:50 +0300

    prismjs

--- _assets/index.js
+++ _assets/index.js
@@ -6,7 +6,6 @@ require('./scss/font-awesome.scss');
 
 // Prism.
 var Prism = require('prismjs');
-require('./scss/prism.scss');
 
 // Prism languages.
 require('prismjs/components/prism-yaml');
@@ -15,17 +14,13 @@ require('prismjs/components/prism-perl');
 require('prismjs/components/prism-bash');
 require('prismjs/components/prism-javascript');
 
-// Prism theme.
-require('prismjs/themes/prism.css');
-
 // Prism plug-ins.
 require('prismjs/plugins/line-numbers/prism-line-numbers');
 require('prismjs/plugins/autolinker/prism-autolinker');
 require('prismjs/plugins/command-line/prism-command-line');
 require('prismjs/plugins/custom-class/prism-custom-class');
-// You must also change _assets/scss/prism.scss in order to change the
-// theme!
-Prism.plugins.customClass.map(require('prismjs/themes/prism.css.json'));
+require('./scss/prism.scss');
+Prism.plugins.customClass.map(require('./scss/prism.scss.json'));
 
 require('./scss/qgoda-default.scss');
 require('./scss/toc.scss');
